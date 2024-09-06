e6728fb67f27a4b190ee51f03d74ada45549439d
lookandhate <princessselestiapwnz@mail.ru>
2024-09-06 23:00:39 +0400

    fix: Consumer interface

---
+++ pkg/message_queue/kafka/client/client.go
@@ -0,0 +1,12 @@
+package client
+
+import (
+	"context"
+
+	"github.com/lookandhate/course_platform_lib/pkg/message_queue/kafka/client/consumer"
+)
+
+type Consumer interface {
+	Consume(ctx context.Context, topicName string, handler consumer.Handler) (err error)
+	Close() error
+}
